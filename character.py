4b4962b5e5b8f60b62feafccb293c125e5b0278b
Hendrik Eerikson <hendrik.eerikson@gmail.com>
2014-11-23 04:23:46 +0200

    Muutsin char, lisasin pilte

--- character.py
+++ character.py
@@ -22,68 +22,11 @@ class Player(Character):
     
     def __init__(self,imgFile):
         Character.__init__(self,imgFile)
+        self.health = 100
 
 
 class Zombie(Character):
     
     def __init__(self,imgFile):
         Character.__init__(self,imgFile)
-
-
-if __name__ == "__main__":
-    pygame.init()
-
-    clock = pygame.time.Clock()
-    
-    pygame.display.set_caption("Player test")
-    
-    screen = pygame.display.set_mode((600,600))
-
-    group = pygame.sprite.Group()
-    eg = pygame.sprite.Group()
-    
-    player = Player("player.png")
-    enemy = Zombie("player.png")
-
-    group.add(player)
-    eg.add(enemy)
-
-    player.rect.x = 300
-    player.rect.y = 300
-    
-    enemy.rect.x = 500
-    enemy.rect.y = 500
-    
-    while 1:
-        ms = clock.tick(60)
-        
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-               pygame.quit()
-               sys.exit(0)
-            elif e.type == pygame.KEYDOWN:
-                if e.key == pygame.K_w:
-                    enemy.speed[1] = -2
-                elif e.key == pygame.K_s:
-                    enemy.speed[1] = 2
-                elif e.key == pygame.K_d:
-                    enemy.speed[0] = 2
-                elif e.key == pygame.K_a:
-                    enemy.speed[0] = -2
-            elif e.type == pygame.KEYUP:
-                enemy.speed[0] = 0
-                enemy.speed[1] = 0
-
-        if pygame.sprite.spritecollide(enemy,group,0):
-            print("yes")
-        screen.fill((0,0,0))
-
-        group.update()
-        eg.update()
-        
-        group.draw(screen)
-        eg.draw(screen)
-        
-        pygame.display.flip()
-    
-    
+        self.health = 50
